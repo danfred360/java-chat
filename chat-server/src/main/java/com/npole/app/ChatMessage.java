e7a5202a13516c814846f3b36a344619fb1d4e6e
Daniel Frederick <dannyfrederick360@gmail.com>
2022-01-13 11:16:18 -0500

    initialized maven structure, generated jars

---
+++ chat-server/src/main/java/com/npole/app/ChatMessage.java
@@ -0,0 +1,24 @@
+package com.npole.app;
+
+import java.io.*;
+
+public class ChatMessage implements Serializable {
+    protected static final long serialVersionUUID = 1112122200L;
+    static final int WHOISIN = 0, MESSAGE = 1, LOGOUT = 2;
+    private int type;
+    private String message;
+    
+    // constructor
+    ChatMessage (int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+    
+    int getType() {
+        return type;
+    }
+    
+    String getMessage() {
+        return message;
+    }
+}
